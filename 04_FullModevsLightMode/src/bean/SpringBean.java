a683224db93f34b278128f9b49294f1dd8107d53
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-05-27 09:31:03 +0530

    Full Modevs Light Mode

---
+++ 04_FullModevsLightMode/src/bean/SpringBean.java
@@ -0,0 +1,18 @@
+package bean;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : Yasiru Dahanayaka
+ * @name : Spring Testing
+ * @date : 5/27/2022
+ * @month : 05
+ * @year : 2022
+ * @since : 0.1.0
+ **/
+@Component
+public class SpringBean {
+    public SpringBean(){
+        System.out.println("Spring Bean One Instantiate");
+    }
+}
