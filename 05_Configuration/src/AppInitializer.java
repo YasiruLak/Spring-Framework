f116c1b9596e35273dc9429e66074ed14db12664
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-05-28 23:20:54 +0530

    Spring Configuration Introduce

---
+++ 05_Configuration/src/AppInitializer.java
@@ -0,0 +1,19 @@
+import config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author : Yasiru Dahanayaka
+ * @name : Spring Testing
+ * @date : 5/28/2022
+ * @month : 05
+ * @year : 2022
+ * @since : 0.1.0
+ **/
+public class AppInitializer {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(AppConfig.class);
+        context.refresh();
+        context.registerShutdownHook();
+    }
+}
---
+++ 05_Configuration/src/bean/SpringBeanOne.java
@@ -0,0 +1,19 @@
+package bean;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : Yasiru Dahanayaka
+ * @name : Spring Testing
+ * @date : 5/28/2022
+ * @month : 05
+ * @year : 2022
+ * @since : 0.1.0
+ **/
+@Component
+public class SpringBeanOne {
+
+    public SpringBeanOne(){
+        System.out.println("Spring Bean One");
+    }
+}
