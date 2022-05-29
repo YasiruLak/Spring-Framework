894ced08e6eb5fc30006d7261ddb1b7cf5a89024
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-05-29 14:47:10 +0530

    Introduce Spring Environment

---
+++ 07_SpringEnvironment/src/bean/SpringBranOne.java
@@ -0,0 +1,18 @@
+package bean;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : Yasiru Dahanayaka
+ * @name : Spring Testing
+ * @date : 5/29/2022
+ * @month : 05
+ * @year : 2022
+ * @since : 0.1.0
+ **/
+@Component
+public class SpringBranOne {
+    public SpringBranOne() {
+        System.out.println("Spring Bean One Instantiated");
+    }
+}
