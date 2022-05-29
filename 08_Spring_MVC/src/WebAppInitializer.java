5e8c1891fa635223c8b3c8e119ff92a1b8be3086
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-05-29 22:02:07 +0530

    Update WebAppInitializer

--- 08_Spring_MVC/src/WebAppInitializer.java
+++ 08_Spring_MVC/src/WebAppInitializer.java
@@ -1,3 +1,7 @@
+import config.WebAppConfig;
+import config.WebRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
 /**
  * @author : Yasiru Dahanayaka
  * @name : Spring Testing
@@ -6,5 +10,24 @@
  * @year : 2022
  * @since : 0.1.0
  **/
-public class WebAppInitializer {
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
