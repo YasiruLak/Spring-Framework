842cdd5e768f8472c7b64ae61ad5864f49d02b61
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-05-29 10:15:08 +0530

    Value Annotation Introduce

--- 06_RuntimeValueInjection/src/bean/SpringBean.java
+++ 06_RuntimeValueInjection/src/bean/SpringBean.java
@@ -9,12 +9,18 @@ package bean;
  * @since : 0.1.0
  **/
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBean {
 
-    public SpringBean() {
+//    public SpringBean() {
+//        System.out.println("Spring Bean Default Constructor");
+//    }
+
+    public SpringBean(@Value("C001") String id) {
         System.out.println("Spring Bean Instantiated");
+        System.out.println(id);
     }
 }
