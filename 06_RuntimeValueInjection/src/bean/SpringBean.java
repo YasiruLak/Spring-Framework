0b0ccc1e7b4bb7a8b16e1e4ae17ed7e07598ed12
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-05-29 10:04:53 +0530

    Runtime Value Added

---
+++ 06_RuntimeValueInjection/src/bean/SpringBean.java
@@ -0,0 +1,20 @@
+package bean;
+
+/**
+ * @author : Yasiru Dahanayaka
+ * @name : Spring Testing
+ * @date : 5/29/2022
+ * @month : 05
+ * @year : 2022
+ * @since : 0.1.0
+ **/
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBean {
+
+    public SpringBean() {
+        System.out.println("Spring Bean Instantiated");
+    }
+}
