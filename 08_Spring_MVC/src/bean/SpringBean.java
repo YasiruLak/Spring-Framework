699677fa9fa1d8a1c30a0c7f6c39fc16b7fa02e3
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-05-29 22:22:38 +0530

    Add Life Cycle Spring MVC

--- 08_Spring_MVC/src/bean/SpringBean.java
+++ 08_Spring_MVC/src/bean/SpringBean.java
@@ -1,5 +1,9 @@
 package bean;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,9 +15,34 @@ import org.springframework.stereotype.Component;
  * @since : 0.1.0
  **/
 @Component
-public class SpringBean {
+public class SpringBean implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
 
     public SpringBean() {
         System.out.println("Spring Bean Instantiated");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("BeanFactory");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("BeanName");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Destroy");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("After Property Set");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Set Application Context");
     }
 }
