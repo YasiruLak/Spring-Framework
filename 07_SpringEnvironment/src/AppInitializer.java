1e5cf74f56e68127ef855faa4e1808721a4bbd3d
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-05-29 15:34:08 +0530

    System Variable Access in Java

--- 07_SpringEnvironment/src/AppInitializer.java
+++ 07_SpringEnvironment/src/AppInitializer.java
@@ -1,6 +1,8 @@
 import config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Map;
+
 /**
  * @author : Yasiru Dahanayaka
  * @name : Spring Testing
@@ -14,6 +16,15 @@ public class AppInitializer {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.register(AppConfig.class);
         context.refresh();
-        context.registerShutdownHook();;
+        context.registerShutdownHook();
+
+
+        //Access System Environment Variables With Java
+        //System.getenv();
+
+        Map<String, String> getenv = System.getenv();
+        for (String key : getenv.keySet()) {
+            System.out.println(key+" : "+getenv.get(key));
+        }
     }
 }
