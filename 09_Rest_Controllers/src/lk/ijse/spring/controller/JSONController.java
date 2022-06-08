10b3eb1fdd2b56fc32f20c8d07aee19153df11eb
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-06-08 21:42:49 +0530

    Added Json Controller

--- 09_Rest_Controllers/src/lk/ijse/spring/controller/JSONController.java
+++ 09_Rest_Controllers/src/lk/ijse/spring/controller/JSONController.java
@@ -1,5 +1,8 @@
 package lk.ijse.spring.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author : Yasiru Dahanayaka
  * @name : Spring Testing
@@ -8,5 +11,7 @@ package lk.ijse.spring.controller;
  * @year : 2022
  * @since : 0.1.0
  **/
+@RestController
+@RequestMapping("json")
 public class JSONController {
 }
