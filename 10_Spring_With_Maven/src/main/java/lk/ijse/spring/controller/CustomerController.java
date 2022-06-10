62dd4f005a985255bcffcd7edb89bc2d7b6efcc8
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-06-10 13:40:35 +0530

    Add Jackson Dependancy

--- 10_Spring_With_Maven/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ 10_Spring_With_Maven/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,5 +1,7 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.dto.CustomerDTO;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("customer")
 public class CustomerController {
 
-    @GetMapping
-    public String getAllCustomer(){
-        return "Hello Maven";
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public CustomerDTO getAllCustomer(){
+        return new CustomerDTO("C001","Yasiru","Galle",100.00);
     }
 }
