81e29f54a95b62d7ccf49bd21cad91af95e68b25
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-06-13 20:07:52 +0530

    Introduce LocalContainerEntityManagerFactoryBean

--- 12_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/WebRootConfig.java
+++ 12_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/WebRootConfig.java
@@ -1,6 +1,9 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 /**
  * @author : Yasiru Dahanayaka
@@ -11,5 +14,15 @@ import org.springframework.context.annotation.Configuration;
  * @since : 0.1.0
  **/
 @Configuration
+@Import(JPAConfig.class)
 public class WebRootConfig {
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean EntityManagerFactory(){
+        LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+        //bean.setJpaVendorAdapter(); //vendor
+        //bean.setDataSource();
+        bean.setPackagesToScan("lk.ijse.spring.entity"); //location where entity
+        return bean;
+    }
 }
