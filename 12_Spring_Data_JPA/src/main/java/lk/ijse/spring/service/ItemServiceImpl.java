3200e9b1a2db770b17fdb190517cecfbc78890ee
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-06-17 19:38:03 +0530

    Item Service Layer Done

---
+++ 12_Spring_Data_JPA/src/main/java/lk/ijse/spring/service/ItemServiceImpl.java
@@ -0,0 +1,46 @@
+package lk.ijse.spring.service;
+
+import lk.ijse.spring.entity.Item;
+import lk.ijse.spring.repo.ItemRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * @author : Yasiru Dahanayaka
+ * @name : Spring Testing
+ * @date : 6/17/2022
+ * @month : 06
+ * @year : 2022
+ * @since : 0.1.0
+ **/
+public class ItemServiceImpl {
+
+    @Autowired
+    private ItemRepo itemRepo;
+
+    public void saveItem(Item item){
+
+        itemRepo.save(item);
+    }
+
+    public void updateItem(Item item){
+
+        itemRepo.save(item);
+    }
+
+    public List<Item> getAllItems(Item item){
+
+        return itemRepo.findAll();
+    }
+
+    public void deleteItem(String id){
+
+        itemRepo.deleteById(id);
+    }
+
+    public Item searchItem(String id){
+
+        return itemRepo.findById(id).get();
+    }
+}
