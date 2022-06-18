d89b8375cca2e63b3541be139d33263c0cb5f991
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-06-18 10:41:29 +0530

    Added Resource Bundle

--- 12_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ 12_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -1,7 +1,10 @@
 package lk.ijse.spring.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -26,31 +29,35 @@ import javax.sql.DataSource;
 @Configuration
 @EnableJpaRepositories(basePackages = "lk.ijse.spring.repo")
 @EnableTransactionManagement
+@PropertySource("classpath:application.properties")
 public class JPAConfig {
+
+    @Autowired
+    Environment environment;
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter va){
         LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
         bean.setJpaVendorAdapter(va); // Vendor (Hibernate)
         bean.setDataSource(ds); //Connection
-        bean.setPackagesToScan("lk.ijse.spring.entity"); // location of the entity
+        bean.setPackagesToScan(environment.getRequiredProperty("entity.package.name")); // location of the entity
         return bean;
     }
 
     @Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource= new DriverManagerDataSource();
-        dataSource.setUrl("jdbc:mysql://localhost:3306/springjpa?createDatabaseIfNotExist=true");
-        dataSource.setUsername("root");
-        dataSource.setPassword("19980611");
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl(environment.getRequiredProperty("my.app.url"));
+        dataSource.setUsername(environment.getRequiredProperty("my.app.username"));
+        dataSource.setPassword(environment.getRequiredProperty("my.app.password"));
+        dataSource.setDriverClassName(environment.getRequiredProperty("my.app.driverclassname"));
         return dataSource;
     }
 
     @Bean
     public JpaVendorAdapter jpaVendorAdapter(){
         HibernateJpaVendorAdapter vendor= new HibernateJpaVendorAdapter();
-        vendor.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+        vendor.setDatabasePlatform(environment.getRequiredProperty("my.app.dialect"));
         vendor.setDatabase(Database.MYSQL);
         vendor.setShowSql(true);
         vendor.setGenerateDdl(true);
