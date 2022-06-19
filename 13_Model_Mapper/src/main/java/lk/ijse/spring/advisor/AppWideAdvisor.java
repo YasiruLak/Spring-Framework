3b1ddb799ef013b8c7f332fdbfb098689c41c9c1
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-06-19 16:28:13 +0530

    Resolve Errors Content Updated

--- 13_Model_Mapper/src/main/java/lk/ijse/spring/advisor/AppWideAdvisor.java
+++ 13_Model_Mapper/src/main/java/lk/ijse/spring/advisor/AppWideAdvisor.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
@@ -19,10 +20,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @CrossOrigin
 public class AppWideAdvisor {
 
+//    @ExceptionHandler({Exception.class})
+//    public ResponseEntity exceptionHandler(Exception e) {
+//        //return new ResponseUtil(500, e.getMessage(), null);
+//        return new ResponseEntity(new ResponseUtil(500, e.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR);
+//    }
+
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({Exception.class})
-    public ResponseEntity ExceptionHandler(Exception e){
-//        return new ResponseUtil(500,e.getMessage(),null);
-        return new ResponseEntity(new ResponseUtil(500,e.getMessage(),null),
-                HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseUtil exceptionHandler(Exception e) {
+        return new ResponseUtil(500, e.getMessage(), null);
     }
 }
